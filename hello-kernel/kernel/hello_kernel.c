4f8049099aeb5c4bafd7b7e6375f065ee354ca40
Dezeli <haterecursive@gmail.com>
2025-11-30 11:07:01 +0900

    1

---
+++ hello-kernel/kernel/hello_kernel.c
@@ -0,0 +1,100 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/uaccess.h>
+#include <linux/ioctl.h>
+
+#include "../include/abi.h"
+
+#define STUDENT_ID 202224210 // Don't modify
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR(TBD);
+MODULE_DESCRIPTION("Hello Kernel ioctl example (IO/IOR/IOW/IOWR + ABI)");
+MODULE_VERSION("0.1");
+
+static dev_t _dev;
+static struct cdev _cdev;
+static struct class *_class;
+
+static int  hello_open   (struct inode *inode, struct file *file);
+static int  hello_release(struct inode *inode, struct file *file);
+static long hello_ioctl  (struct file *file, unsigned int cmd, unsigned long arg);
+
+static int  __init hello_init(void);
+static void __exit hello_exit(void);
+
+static const struct file_operations _fops = {
+    .owner          = THIS_MODULE,
+    .open           = hello_open,
+    .release        = hello_release,
+    .unlocked_ioctl = hello_ioctl,
+};
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+static int current_id = STUDENT_ID;
+
+static int hello_open(struct inode *inode, struct file *file) {
+    pr_info("hello_kernel: device opened\n");
+    return 0;
+}
+
+static int hello_release(struct inode *inode, struct file *file) {
+    pr_info("hello_kernel: device closed\n");
+    return 0;
+}
+
+static long hello_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
+    switch (cmd) {
+
+    case IOCTL_PRINT:    /* _IO */
+
+
+        // to be implemented
+
+
+        return 0;
+
+    case IOCTL_GET_ID:   /* _IOR */
+
+
+        // to be implemented
+
+
+        return 0;
+
+    case IOCTL_SET_ID:   /* _IOW */
+
+
+        // to be implemented
+
+        
+        return 0;
+
+    // case ... : /* _IOWR */
+
+
+    //     return 0;
+
+    default:
+
+        return -ENOTTY; /* 지원하지 않는 ioctl */
+
+    }
+}
+
+static int __init hello_init(void) {
+
+    // to be implemented
+
+}
+
+static void __exit hello_exit(void) {
+
+    // to be implemented
+
+}
